fca50788fdc3324efa440688abc327c37c0e55d8
Emil Darakchiev <72097603+edarakchiev@users.noreply.github.com>
2021-01-31 11:28:22 +0200

    Add files via upload

---
+++ Exercise/03.min_max_sum.py
@@ -0,0 +1,8 @@
+def min_max_sum(numbers):
+    print(f"The minimum number is {min(numbers)}")
+    print(f"The maximum number is {max(numbers)}")
+    print(f"The sum number is: {sum(numbers)}")
+
+
+nums = [int(el) for el in input().split()]
+min_max_sum(nums)
